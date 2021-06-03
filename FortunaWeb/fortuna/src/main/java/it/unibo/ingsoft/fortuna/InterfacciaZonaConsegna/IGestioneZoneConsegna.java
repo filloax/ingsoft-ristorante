3cfc85cdf12eef0e3b8cf0cbc502fd7dae7ed51c
lorenzorambo <lorenzo.rambelli99@gmail.com>
2021-06-03 15:25:07 +0200

    aggiunte zon cons base

---
+++ FortunaWeb/fortuna/src/main/java/it/unibo/ingsoft/fortuna/InterfacciaZonaConsegna/IGestioneZoneConsegna.java
@@ -0,0 +1,13 @@
+package it.unibo.ingsoft.fortuna.InterfacciaZonaConsegna;
+import java.util.Set;
+import java.util.List;
+
+import it.unibo.ingsoft.fortuna.model.ZonaConsegna;
+
+public interface IGestioneZoneConsegna {
+    //public void aggiungZonaConsegna(List<Pair<Double,Double>>, double prezzoMinimo);
+    public void rimuoviZonaConsegna(ZonaConsegna toRemove);
+    public Set<ZonaConsegna> listaZoneConsegna();
+
+
+}
---
+++ FortunaWeb/fortuna/src/main/java/it/unibo/ingsoft/fortuna/model/ZonaConsegna.java
@@ -0,0 +1,5 @@
+package it.unibo.ingsoft.fortuna.model;
+
+public class ZonaConsegna {
+    
+}
