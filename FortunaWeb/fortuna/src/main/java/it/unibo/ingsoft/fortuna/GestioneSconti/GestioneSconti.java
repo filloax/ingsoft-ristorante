ea3a90fec2f429adbd78b940d613219cac872beb
lorenzorambo <lorenzo.rambelli99@gmail.com>
2021-06-06 20:39:06 +0200

    robe gest sconti

--- FortunaWeb/fortuna/src/main/java/it/unibo/ingsoft/fortuna/GestioneSconti/GestioneSconti.java
+++ FortunaWeb/fortuna/src/main/java/it/unibo/ingsoft/fortuna/GestioneSconti/GestioneSconti.java
@@ -19,65 +19,111 @@ public class GestioneSconti extends Controller implements IGestioneSconti {
 
     @Override
     public void aggiungiSconto(LocalDateTime start, LocalDateTime end, double quantita, boolean isPercent) {
-        // TODO Auto-generated method stub
+        Sconto toAdd = new Sconto();
+        if(isPercent)
+            toAdd = Sconto.of(start, end, quantita, 0, 0);
+        else
+            toAdd = Sconto.of(start, end, 0, quantita, 0);
         
+        sconti.add(toAdd);
+        //aggiunta di toAdd al DB
     }
 
     @Override
     public void aggiungiSconto(LocalDateTime start, LocalDateTime end, double quantita, boolean isPercent, Prodotto per,
-            double prezzoMinimo) {
-        // TODO Auto-generated method stub
+        double prezzoMinimo) {
+        Sconto toAdd = new Sconto();
+        if(isPercent)
+            toAdd = Sconto.ofProdotti(start, end, quantita, 0, prezzoMinimo, per);
+        else
+        toAdd = Sconto.ofProdotti(start, end, 0, quantita, prezzoMinimo, per);
         
+        sconti.add(toAdd);
+       //aggiunta di toAdd al DB 
     }
 
     @Override
     public void aggiungiSconto(LocalDateTime start, LocalDateTime end, double quantita, boolean isPercent,
             Prodotto per) {
-        // TODO Auto-generated method stub
+        Sconto toAdd = new Sconto();
+        if(isPercent)
+            toAdd = Sconto.ofProdotti(start, end, quantita, 0, 0, per);
+        else
+        toAdd = Sconto.ofProdotti(start, end, 0, quantita, 0, per);
         
+        sconti.add(toAdd);
+        //aggiunta di toAdd al DB
     }
 
     @Override
     public void aggiungiSconto(LocalDateTime start, LocalDateTime end, double quantita, boolean isPercent,
             double prezzoMinimo) {
-        // TODO Auto-generated method stub
+        Sconto toAdd = new Sconto();
+        if(isPercent)
+            toAdd = Sconto.of(start, end, quantita, 0, prezzoMinimo);
+        else
+            toAdd = Sconto.of(start, end, 0, quantita, prezzoMinimo);
         
+        sconti.add(toAdd);
+        //aggiunta di toAdd al DB
     }
 
     @Override
     public void rimuoviSconto(Sconto toRemove) {
-        // TODO Auto-generated method stub
-        
+        sconti.remove(toRemove);
+        //rimuovi toRemove dal DB
     }
 
     @Override
     public Set<Sconto> listaSconti(LocalDateTime start, LocalDateTime end) {
-        // TODO Auto-generated method stub
-        return null;
+        Set<Sconto> out = new HashSet<Sconto>();
+        for (Sconto sconto : sconti) {
+            if(sconto.getInizio().equals(start) && sconto.getFine().equals(end))
+            out.add(sconto);
+        }
+        return out;
     }
 
     @Override
     public Set<Sconto> listaSconti(LocalDateTime start, LocalDateTime end, Prodotto per) {
-        // TODO Auto-generated method stub
-        return null;
+        Set<Sconto> out = new HashSet<Sconto>();
+        for (Sconto sconto : sconti) {
+            if(sconto.getInizio().equals(start)
+                && sconto.getFine().equals(end)
+                && sconto.getPerProdotti().contains(per))
+            out.add(sconto);
+        }
+        return out;
     }
 
     @Override
     public Set<Sconto> listaSconti(LocalDateTime start, LocalDateTime end, double prezzoMin) {
-        // TODO Auto-generated method stub
-        return null;
+        Set<Sconto> out = new HashSet<Sconto>();
+        for (Sconto sconto : sconti) {
+            if(sconto.getInizio().equals(start) 
+                && sconto.getFine().equals(end)
+                && sconto.getCostoMinimo() == prezzoMin)
+            out.add(sconto);
+        }
+        return out;
     }
 
     @Override
     public Set<Sconto> listaSconti(LocalDateTime start, LocalDateTime end, Prodotto per, double prezzoMin) {
-        // TODO Auto-generated method stub
-        return null;
+        Set<Sconto> out = new HashSet<Sconto>();
+        for (Sconto sconto : sconti) {
+            if(sconto.getInizio().equals(start)
+                && sconto.getFine().equals(end)
+                && sconto.getPerProdotti().contains(per)
+                && sconto.getCostoMinimo() == prezzoMin)
+            out.add(sconto);
+        }
+        return out;
     }
 
     @Override
     public Set<Sconto> listaScontiTotali() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.sconti;
     }
     
 }
