ff76572847f4f09e7fae09bab47c6edd51f0306e
Filloax <filloax@gmail.com>
2021-06-09 09:33:56 +0200

    test zonaconsegna mentre aspetto il mio turno a Diritto Informatico

---
+++ FortunaWeb/fortuna/src/test/java/it/unibo/ingsoft/fortuna/model/zonaconsegna/ZonaConsegnaTest.java
@@ -0,0 +1,58 @@
+package it.unibo.ingsoft.fortuna.model.zonaconsegna;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ZonaConsegnaTest {
+    private ZonaConsegna zonaConsegna1;
+    private ZonaConsegna zonaConsegna2;
+    private List<Vector> puntiRettangolo;
+    private List<Vector> puntiTrapezio;
+
+    @BeforeEach
+    public void beforeEach() {
+        puntiRettangolo = List.of(new Vector(0, 0), new Vector(5, 0), new Vector(5, 2), new Vector(0, 2));
+        puntiTrapezio= List.of(new Vector(0, 0), new Vector(5, 0), new Vector(2, 3), new Vector(0, 2));
+
+        
+        zonaConsegna1 = new ZonaConsegna(0.0, puntiRettangolo);
+        zonaConsegna2 = new ZonaConsegna(5.0, puntiTrapezio);
+    }
+
+    @Test
+    public void testShapeBase() throws ZonaConsegnaException {
+        assertTrue(zonaConsegna1.include(new Vector(3, 1), 99));
+        assertTrue(zonaConsegna2.include(new Vector(3, 1), 99));
+        assertFalse(zonaConsegna1.include(new Vector(-1, 1), 99));
+        assertFalse(zonaConsegna2.include(new Vector(-1, 1), 99));
+        assertTrue(zonaConsegna1.include(new Vector(4, 1), 99));
+        assertFalse(zonaConsegna2.include(new Vector(4.5, 1), 99));
+    }
+    
+    @Test
+    public void testShapeLati() throws ZonaConsegnaException {
+        assertTrue(zonaConsegna1.include(new Vector(0, 1), 99));
+        assertTrue(zonaConsegna1.include(new Vector(5, 1), 99));
+        assertTrue(zonaConsegna1.include(new Vector(3, 2), 99));
+        assertTrue(zonaConsegna1.include(new Vector(3, 0), 99));
+
+        assertTrue(zonaConsegna2.include(new Vector(0, 1), 99));
+        assertTrue(zonaConsegna2.include(new Vector(4, 1), 99));
+        assertTrue(zonaConsegna2.include(new Vector(2, 2), 99));
+        assertTrue(zonaConsegna2.include(new Vector(3, 0), 99));
+    }
+
+    @Test
+    public void testShapeVertici() throws ZonaConsegnaException {
+        for (Vector p : puntiRettangolo)
+            assertTrue(zonaConsegna1.include(p, 99));
+        for (Vector p : puntiTrapezio)
+            assertTrue(zonaConsegna2.include(p, 99));
+    }
+}
