678a5ab64163048f282e1238e6dfff88b02f2150
lorenzorambo <lorenzo.rambelli99@gmail.com>
2021-06-10 19:31:01 +0200

    boh

---
+++ fortunagest/src/main/java/viewModificaProdotti/ModificaProdottiView.java
@@ -0,0 +1,15 @@
+package main.java.viewModificaProdotti;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+
+public class ModificaProdottiView extends Application {
+    
+}
