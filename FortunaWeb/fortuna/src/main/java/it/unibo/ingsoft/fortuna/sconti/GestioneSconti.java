cd71c5735a3f4d0ad153ae0f71788a6451285bd9
Filloax <filloax@gmail.com>
2021-06-11 10:44:10 +0200

    Impostato formatter

--- FortunaWeb/fortuna/src/main/java/it/unibo/ingsoft/fortuna/sconti/GestioneSconti.java
+++ FortunaWeb/fortuna/src/main/java/it/unibo/ingsoft/fortuna/sconti/GestioneSconti.java
@@ -21,64 +21,65 @@ public class GestioneSconti extends Controller implements IGestioneSconti {
 
     public GestioneSconti() {
         sconti = new HashSet<Sconto>();
-        //scarica "sconti" da database
+        // scarica "sconti" da database
     }
 
     @Override
-    public Sconto aggiungiSconto(LocalDateTime start, LocalDateTime end, double quantita, boolean isPercent) {
+    public Sconto aggiungiSconto(LocalDateTime start, LocalDateTime end, double quantita,
+            boolean isPercent) {
         Sconto toAdd = new Sconto();
         if (isPercent)
             toAdd = Sconto.of(start, end, quantita, 0, 0);
         else
             toAdd = Sconto.of(start, end, 0, quantita, 0);
-        
+
         sconti.add(toAdd);
-        //aggiunta di toAdd al DB
+        // aggiunta di toAdd al DB
 
         return toAdd;
     }
 
     @Override
-    public Sconto aggiungiSconto(LocalDateTime start, LocalDateTime end, double quantita, boolean isPercent, 
-    double prezzoMinimo, Prodotto perProdotto) {
+    public Sconto aggiungiSconto(LocalDateTime start, LocalDateTime end, double quantita,
+            boolean isPercent, double prezzoMinimo, Prodotto perProdotto) {
         Sconto toAdd = new Sconto();
         if (isPercent)
             toAdd = Sconto.ofProdotti(start, end, quantita, 0, prezzoMinimo, perProdotto);
         else
             toAdd = Sconto.ofProdotti(start, end, 0, quantita, prezzoMinimo, perProdotto);
-        
-        sconti.add(toAdd);
-        //aggiunta di toAdd al DB 
 
-       return toAdd;
-    }
-
-    @Override
-    public Sconto aggiungiSconto(LocalDateTime start, LocalDateTime end, double quantita, boolean isPercent,
-            Prodotto perProdotto) {
-        Sconto toAdd = new Sconto();
-        if (isPercent)
-            toAdd = Sconto.ofProdotti(start, end, quantita, 0, 0, perProdotto);
-        else
-            toAdd = Sconto.ofProdotti(start, end, 0, quantita, 0, perProdotto);
-        
+        // aggiunta di toAdd al DB
         sconti.add(toAdd);
-        //aggiunta di toAdd al DB
 
         return toAdd;
     }
 
     @Override
-    public Sconto aggiungiSconto(LocalDateTime start, LocalDateTime end, double quantita, boolean isPercent,
-            double prezzoMinimo) {
+    public Sconto aggiungiSconto(LocalDateTime start, LocalDateTime end, double quantita,
+            boolean isPercent, Prodotto perProdotto) {
+        Sconto toAdd = new Sconto();
+        if (isPercent)
+            toAdd = Sconto.ofProdotti(start, end, quantita, 0, 0, perProdotto);
+        else
+            toAdd = Sconto.ofProdotti(start, end, 0, quantita, 0, perProdotto);
+
+        // aggiunta di toAdd al DB
+        sconti.add(toAdd);
+
+        return toAdd;
+    }
+
+    @Override
+    public Sconto aggiungiSconto(LocalDateTime start, LocalDateTime end, double quantita,
+            boolean isPercent, double prezzoMinimo) {
         Sconto toAdd = new Sconto();
         if (isPercent)
             toAdd = Sconto.of(start, end, quantita, 0, prezzoMinimo);
         else
             toAdd = Sconto.of(start, end, 0, quantita, prezzoMinimo);
-        
+
         sconti.add(toAdd);
-        //aggiunta di toAdd al DB
+        // aggiunta di toAdd al DB
 
         return toAdd;
     }
@@ -86,49 +87,44 @@ public class GestioneSconti extends Controller implements IGestioneSconti {
     @Override
     public boolean rimuoviSconto(Sconto toRemove) {
         sconti.remove(toRemove);
-        //rimuovi toRemove dal DB
+        // rimuovi toRemove dal DB
 
         return true;
     }
 
     @Override
     public List<Sconto> listaSconti(LocalDateTime inizio, LocalDateTime fine) {
-        return sconti.stream()
-            .filter(sconto -> sconto.overlapsTempo(inizio, fine))
-            .collect(Collectors.toList());
+        return sconti.stream().filter(sconto -> sconto.overlapsTempo(inizio, fine))
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Sconto> listaSconti(LocalDateTime tempo) {
-        return sconti.stream()
-            .filter(sconto -> sconto.isInTempo(tempo))
-            .collect(Collectors.toList());
+        return sconti.stream().filter(sconto -> sconto.isInTempo(tempo))
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Sconto> listaSconti(LocalDateTime tempo, Prodotto perProdotto) {
-        return sconti.stream()
-            .filter(sconto -> sconto.isAttivo(tempo, perProdotto, 0))
-            .collect(Collectors.toList());
+        return sconti.stream().filter(sconto -> sconto.isAttivo(tempo, perProdotto, 0))
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Sconto> listaSconti(LocalDateTime tempo, double prezzoMin) {
-        return sconti.stream()
-            .filter(sconto -> sconto.isAttivo(tempo, 0))
-            .collect(Collectors.toList());
+        return sconti.stream().filter(sconto -> sconto.isAttivo(tempo, 0))
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Sconto> listaSconti(LocalDateTime tempo, double prezzoMin, Prodotto perProdotto) {
-        return sconti.stream()
-            .filter(sconto -> sconto.isAttivo(tempo, perProdotto, prezzoMin))
-            .collect(Collectors.toList());
+        return sconti.stream().filter(sconto -> sconto.isAttivo(tempo, perProdotto, prezzoMin))
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<Sconto> listaScontiTotali() {
         return List.copyOf(sconti);
     }
-    
+
 }
