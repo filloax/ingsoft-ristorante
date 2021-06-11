c1c629987a6faaa5fc5aca5151a159e91bb49e30
Filloax <filloax@gmail.com>
2021-06-11 10:38:38 +0200

    fix a script caricamento key

--- FortunaWeb/fortuna/secret/set-env.ps1
+++ FortunaWeb/fortuna/secret/set-env.ps1
@@ -1,4 +1,11 @@
-$env:DB_SPRINGUSER_PW = $(cat db.springuser.p)
-$env:GEOCODING_KEY = $(cat google.geocoding.key)
-$env:STRIPE_SECRET_KEY = $(cat stripe.key)
-$env:STRIPE_PUBLIC_KEY = $(cat stripe.pub.key)
+$origdir = Get-Location
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+cd $dir
+
+$Env:DB_SPRINGUSER_PW = $(cat db.springuser.p)
+$Env:GEOCODING_KEY = $(cat google.geocoding.key)
+$Env:STRIPE_SECRET_KEY = $(cat stripe.key)
+$Env:STRIPE_PUBLIC_KEY = $(cat stripe.pub.key)
+
+cd $origdir
--- FortunaWeb/fortuna/secret/set-env.sh
+++ FortunaWeb/fortuna/secret/set-env.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+ORIG_DIR=$(pwd)
+PERC=$(realpath "$0")
+DIR=$(dirname "$PERC")
+cd "$DIR"
+
 export DB_SPRINGUSER_PW=$(cat db.springuser.p)
 export GEOCODING_KEY=$(cat google.geocoding.key)
 export STRIPE_SECRET_KEY=$(cat stripe.key)
 export STRIPE_PUBLIC_KEY=$(cat stripe.pub.key)
+
+cd "$ORIG_DIR"
