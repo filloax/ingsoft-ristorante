5f1dc409f307c23d745610e6d9cd35845354a0d5
Filloax <filloax@gmail.com>
2021-06-14 17:29:04 +0200

    sms mock ha un output

--- FortunaWeb/fortuna/src/main/java/it/unibo/ingsoft/fortuna/sms/InvioSMSMock.java
+++ FortunaWeb/fortuna/src/main/java/it/unibo/ingsoft/fortuna/sms/InvioSMSMock.java
@@ -7,6 +7,7 @@ public class InvioSMSMock implements IInvioSms {
 
     @Override
     public void inviaSMS(String telefono, String messaggio) throws SMSException {
+        System.out.println("INVIATO SMS @" + telefono + ": " + messaggio);
     }
     
 }
