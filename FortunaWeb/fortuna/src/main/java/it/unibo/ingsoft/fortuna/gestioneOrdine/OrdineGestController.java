55c71237dfb01a9c472872ee2e075a6de75a95cb
HSCnoKenju <26322756+HSCnoKenju@users.noreply.github.com>
2021-06-15 13:00:11 +0200

    finisce RESTapi gestioneOrdine lato server
    Da fare delete, essendo più associazioni molti a molti bisogna
    aggiornare più tabelle in una volta sola

--- FortunaWeb/fortuna/src/main/java/it/unibo/ingsoft/fortuna/gestioneOrdine/OrdineGestController.java
+++ FortunaWeb/fortuna/src/main/java/it/unibo/ingsoft/fortuna/gestioneOrdine/OrdineGestController.java
@@ -1,7 +1,10 @@
 package it.unibo.ingsoft.fortuna.gestioneOrdine;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.NoSuchElementException;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,15 +13,19 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import it.unibo.ingsoft.fortuna.AbstractController;
+import it.unibo.ingsoft.fortuna.DatabaseException;
 import it.unibo.ingsoft.fortuna.model.richiesta.Ordine;
+import it.unibo.ingsoft.fortuna.sms.SMSException;
 
 @RestController
 @RequestMapping("/gest-ordini")
-public class OrdineGestController {
+public class OrdineGestController extends AbstractController {
 
     @Autowired
     private OrdineGestService service;
@@ -39,28 +46,54 @@ public class OrdineGestController {
 
     }
 
-    @PostMapping
-    public void add(@RequestBody Ordine ordine) {
-        service.save(ordine);
-        // TODO:
-        // Log
-        // NotificaSMS
-    }
+    // @PostMapping
+    // public void add(@RequestBody Ordine ordine) {
+    // service.save(ordine);
+    // // TODO:
+    // // Log
+    // // NotificaSMS
+    // }
 
-    @DeleteMapping(value = "{id}")
-    public ResponseEntity<?> delete(@PathVariable Integer id) {
+    @PutMapping(value = "{id}")
+    public ResponseEntity<?> accetta(HttpServletRequest request, @PathVariable Integer id) {
+        // controllo che la prenotazione esiste in database
+        /*
+         * è implicito il fatto che se passo dal URI ~/10, l'oggetto json che passo ha
+         * id 10, non si sa perchè non c'è un controllo su questo (potenzialmente uno
+         * può fare una richiesta ad ~/10 e aggiornare l'oggetto con id 9)
+         * 
+         */
 
-        // TODO:
-        // Log
-        // NotificaSMS
+        scriviOperazione(request.getRemoteAddr(), String.format("accettaOrdine(id: %d)", id));
+
         try {
-
-            Ordine validOrdine = service.get(id);
-            service.delete(id);
+            service.accetta(id);
             return new ResponseEntity<>(HttpStatus.OK);
 
         } catch (NoSuchElementException e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (DatabaseException | SMSException | IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    @DeleteMapping(value = "{id}")
+    public ResponseEntity<?> delete(HttpServletRequest request, @PathVariable Integer id) {
+        scriviOperazione(request.getRemoteAddr(), String.format("deleteOrdine(id: %d)", id));
+
+        String ragione = "";
+
+        try {
+            service.cancella(id, ragione);
+            return new ResponseEntity<>(HttpStatus.OK);
+
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (SMSException | IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
--- FortunaWeb/fortuna/src/main/java/it/unibo/ingsoft/fortuna/gestioneOrdine/OrdineGestRepository.java
+++ FortunaWeb/fortuna/src/main/java/it/unibo/ingsoft/fortuna/gestioneOrdine/OrdineGestRepository.java
@@ -3,6 +3,7 @@ package it.unibo.ingsoft.fortuna.gestioneOrdine;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
 import it.unibo.ingsoft.fortuna.model.Prodotto;
 import it.unibo.ingsoft.fortuna.model.ProdottoOrdine;
@@ -14,12 +15,23 @@ public interface OrdineGestRepository extends JpaRepository<Ordine, Integer> {
     @Query(value = "SELECT * FROM ordini", nativeQuery = true)
     List<Ordine> fetchTutti();
 
-
-    @Query(value="SELECT * FROM ordini WHERE id=?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM ordini WHERE id=?1", nativeQuery = true)
     List<Ordine> fetchUno(Integer ordineId);
 
-   
+    // TODO workaround , dovrei gestire le dipendenze nelle molti a molti di sconti
+    // e prodotti
+    @Modifying
+    @Query(value = "DELETE FROM ordini WHERE id=?1", nativeQuery = true)
+    void deleteOrdine(Integer ordineId);
 
-    @Query(value = "SELECT * FROM prodotti_ordinati WHERE id_ordine = ?1", nativeQuery = true)
-    List<ProdottoOrdine> fetchQuantitaPerOrdine(Integer ordineId);
+    @Query(value = "SELECT * FROM ordini o WHERE o.accettato IS NULL", nativeQuery = true)
+    List<Ordine> findInAttesa();
+
+    @Query(value = "SELECT * FROM ordini o WHERE o.accettato = 'Y'", nativeQuery = true)
+    List<Ordine> findAccettati();
+
+    @Modifying
+    @Query(value = "UPDATE ordini o SET o.accettato='Y' WHERE o.id =?1", nativeQuery = true)
+    int accetta(Integer id);
+
 }
